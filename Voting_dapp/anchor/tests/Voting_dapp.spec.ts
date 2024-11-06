1571306f8d6c7268f7af9b3b2e771e21be106240
AdityaCubicles45 <67513926+AdityaCubicles45@users.noreply.github.com>
2024-11-07 02:36:43 +0530

    test update

--- Voting_dapp/anchor/tests/Voting_dapp.spec.ts
+++ Voting_dapp/anchor/tests/Voting_dapp.spec.ts
@@ -1,10 +1,10 @@
 import * as anchor from '@coral-xyz/anchor'
 import {Program} from '@coral-xyz/anchor'
 import {Keypair, PublicKey} from '@solana/web3.js'
-import {Voting} from '../target/types/voting'
+import {VotingDapp} from '../target/types/Voting_dapp'
 import { BankrunProvider, startAnchor } from "anchor-bankrun";
 
-const IDL = require('../target/idl/voting.json');
+const IDL = require('../target/idl/voting_dapp.json');
 
 const votingAddress = new PublicKey("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
 
@@ -15,17 +15,30 @@ describe('Voting', () => {
     const context = await startAnchor("", [{name: "voting", programId: votingAddress}], []);
 	  const provider = new BankrunProvider(context);
 
-    const votingProgram = new Program<Voting>(
+    const votingProgram = new Program<VotingDapp>(
       IDL,
       provider,
     );
 
     await votingProgram.methods.initializePoll(
-      new anchor.BN(1),
-      "What is favorite type of smartphone?",
-      new anchor.BN(0),
-      new anchor.BN(1830925334),
+      new anchor.BN(1) as anchor.BN,                    
+      "What is your favorite type of smartphone?" as string,
+      new anchor.BN(0) as anchor.BN,                     
+      new anchor.BN(1830925334) as anchor.BN             
     ).rpc();
+    
+    const [pollAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, 'le', 8)],
+      votingAddress,
+    );
+
+    const poll = await votingProgram.account.poll.fetch(pollAddress);
+
+    console.log(poll);
+
+    expect(poll.pollId.toNumber()).toEqual(1);
+    expect(poll.description).toEqual("What is your favorite type of smartphone?");
+    expect(poll.pollStart.toNumber()).toBeLessThan(poll.pollEnd.toNumber());
 
   });
 });
