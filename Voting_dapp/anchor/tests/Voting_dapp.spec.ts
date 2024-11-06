81c147f15babfbdd900a9368ae201daf1ed33f87
AdityaCubicles45 <67513926+AdityaCubicles45@users.noreply.github.com>
2024-11-07 02:09:59 +0530

    test contract created

--- Voting_dapp/anchor/tests/Voting_dapp.spec.ts
+++ Voting_dapp/anchor/tests/Voting_dapp.spec.ts
@@ -1,15 +1,26 @@
 import * as anchor from '@coral-xyz/anchor'
 import {Program} from '@coral-xyz/anchor'
-import {Keypair} from '@solana/web3.js'
+import {Keypair, PublicKey} from '@solana/web3.js'
 import {Voting} from '../target/types/voting'
+import { BankrunProvider, startAnchor } from "anchor-bankrun";
 
 const IDL = require('../target/idl/voting.json');
+
+const votingAddress = new PublicKey("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
 
 
 describe('Voting', () => {
   
   it('Initialize Poll', async () => {
+    const context = await startAnchor("", [{name: "voting", programId: votingAddress}], []);
+	  const provider = new BankrunProvider(context);
 
+    const votingProgram = new Program<Voting>(
+      IDL,
+      provider,
+    );
+
+    await votingProgram.methods
   });
 });
 
