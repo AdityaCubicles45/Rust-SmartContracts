11e1cb31ffd6834b0e641ada1d442628c842c7d8
AdityaCubicles45 <67513926+AdityaCubicles45@users.noreply.github.com>
2024-11-09 01:01:30 +0530

    test contract updation

--- Voting_dapp/anchor/tests/Voting_dapp.spec.ts
+++ Voting_dapp/anchor/tests/Voting_dapp.spec.ts
@@ -10,21 +10,28 @@ const votingAddress = new PublicKey("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZ
 
 
 describe('Voting', () => {
-  
-  it('Initialize Poll', async () => {
-    const context = await startAnchor("", [{name: "voting", programId: votingAddress}], []);
-	  const provider = new BankrunProvider(context);
 
-    const votingProgram = new Program<VotingDapp>(
+  let context;
+  let provider;
+  let votingProgram: anchor.Program<VotingDapp>;
+
+  beforeAll( async () => {
+    context = await startAnchor("", [{name: "voting", programId: votingAddress}], []);
+    provider = new BankrunProvider(context);
+
+    votingProgram = new Program<VotingDapp>(
       IDL,
       provider,
     );
 
+  })
+  
+  it('Initialize Poll', async () => {
     await votingProgram.methods.initializePoll(
-      new anchor.BN(1) as anchor.BN,                    
-      "What is your favorite type of smartphone?" as string,
-      new anchor.BN(0) as anchor.BN,                     
-      new anchor.BN(1830925334) as anchor.BN             
+      new anchor.BN(1),                 
+      "What is your favorite type of smartphone?",
+      new anchor.BN(0),                     
+      new anchor.BN(1830925334),             
     ).rpc();
     
     const [pollAddress] = PublicKey.findProgramAddressSync(
@@ -39,6 +46,35 @@ describe('Voting', () => {
     expect(poll.pollId.toNumber()).toEqual(1);
     expect(poll.description).toEqual("What is your favorite type of smartphone?");
     expect(poll.pollStart.toNumber()).toBeLessThan(poll.pollEnd.toNumber());
+
+  });
+
+  it("initialize candidate", async () => {
+    await votingProgram.methods.initializeCandidate(
+      "iPhone",
+      new anchor.BN(1),                 
+    ).rpc();
+
+    await votingProgram.methods.initializeCandidate(
+      "Samsung",
+      new anchor.BN(1),                 
+    ).rpc();
+
+    const [pollAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(0).toArrayLike(Buffer, 'le', 8)],
+      votingAddress,
+    );
+
+    const poll = await votingProgram.account.poll.fetch(pollAddress);
+
+    console.log(poll);
+
+    expect(poll.candidates[0].name).toEqual("iPhone");
+    expect(poll.candidates[1].name).toEqual("Samsung");
+
+  });
+
+  it("vote", async () => {
 
   });
 });
